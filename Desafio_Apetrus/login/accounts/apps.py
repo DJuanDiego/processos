cc41be5eaa540fe90340e6283b89f1debf658116
DJuanDiego <juan.martins.cruz@gmail.com>
2020-07-14 14:10:30 -0300

    Realocação do Desafio Apetrus.

--- Desafio_Apetrus/login/accounts/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from .models import Alunos
-
-class AccountsConfig(AppConfig):
-    name = 'accounts'
-
-class AlunosConfig(AppConfig):
-    name = 'alunos'
--- Desafio_Apetrus/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/juan/PycharmProjects/Desafio_Apetrus/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
